12a24199d6a2794f60df1ccd6faf805aed05c0c6
jards1 <jarryd@dgtl.im>
2019-03-13 17:28:17 +0800

    loop inside input #inprogress

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,5 +6,53 @@ var xhr = new  XMLHttpRequest();
 var parseJson = false;
 
 $(function (){
+
+    var loopFirstContent = function(){
+        $.each(parseJson, function(key, value){
+            if(key == 'methodResponse'){
+                firstContent = value.content.split('_').join(') ');
+                firstArray = firstContent.split('|');
+                console.log(firstArray);
+                for (i = 0; i < firstArray.length; i++){
+                    
+                }
+            }
+        })
+    }
+
+    var getFirstMenu = function(){
+        var dataToPass = {
+            "method":"select",
+                "data":{
+                    "table":"keyword",
+                    "returnID":"0"
+                }
+        }
+        $.ajax({
+            type:"POST",
+            url: apiUrl,
+            contentType: "json",
+            data : JSON.stringify(dataToPass
+            ),
+            beforeSend: function(xhr){
+                xhr.setRequestHeader ("Basic", btoa(username + ":" + password));
+                xhr.setRequestHeader ("Action", actionPoint);
+            },
+            success : function(response){
+                parseData = JSON.parse(response);
+                parseJson = parseData;
+                console.log(parseJson);
+                loopFirstContent();
+
+            },
+            error: function(textStatus,errorThrown){
+                alert('Network connection error, Reload page');
+                location.reload();
+            },
+            async: false
+        });
+    }
     
+    getFirstMenu();
+
 })
